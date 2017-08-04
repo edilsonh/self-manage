1f74d6ca75bcf955c2dad4702c7639e13eed9cce
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-08-04 14:14:30 -0400

    added a login page, no authentication yet...

--- app.js
+++ app.js
@@ -6,7 +6,50 @@ const app = express()
 const indexRoute = require("./routes/index")
 const userRoute = require("./routes/users")
 const addRoute = require("./routes/add")
+const loginRoute = require("./routes/login")
 const bodyParser = require("body-parser")
+const passport = require('passport'),
+ LocalStrategy = require('passport-local').Strategy;
+const session = require("express-session")
+const flash = require("express-flash-messages")
+
+app.use(session ({
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        User.authenticate(username, password, function(err, user) {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, user)
+            } else {
+                return done(null, false, {
+                    message: "There is no user with that username and password."
+                })
+            }
+        })
+    }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+
 
 app.engine("mustache", mustacheExpress())
 app.set("views", "./views")
@@ -18,6 +61,7 @@ app.use(express.static(path.join(__dirname, "public")))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(indexRoute)
+app.use(loginRoute)
 app.use(addRoute)
 app.use(userRoute)
 
